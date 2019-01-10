06a619964def12f7c12454abedc7fb82725a4bed
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-01-10 15:06:05 +0100

    added bootable state

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,12 @@
 import { PlatformClient } from './lib/platform-client';
-import { Bootable } from './lib/bootable';
+import { Bootable, BootableState } from './lib/bootable';
 import { Channel } from './lib/channel';
 import { Command } from './lib/command';
 import { Message } from './lib/message';
 
 export {
   Bootable,
+  BootableState,
   Channel,
   Command,
   Message,
