f093d1699e81d0d4af399ca8730761e7eb675dc5
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-01-10 16:47:23 +0100

    added command utilities

--- lib/channel.d.ts
+++ lib/channel.d.ts
@@ -1,9 +1,33 @@
 import { PlatformClient } from "./platform-client";
 
+import { Command } from './command';
+
 /**
  * Represents a channel in a messaging service.
  */
 export interface Channel {
+  /**
+   * Adds a command to the channel.
+   * 
+   * The provided constructor function may not require any parameters.
+   * The newly created Command shall be booted by the Channel instance.
+   * @param command 
+   */
+  addCommand(command: new() => Command): Channel;
+
+  /**
+   * Removes a command instance to the channel.
+   *
+   * The Channel shall invoke the teardown method on the Command.
+   * @param command 
+   */
+  removeCommand(command: new() => Command): Channel;
+
+  /**
+   * Returns all commands available/registered in the channel.
+   */
+  commands(): Array<Command>;
+
   /**
    * Join the current channel.
    */
